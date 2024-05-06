555caf5aa520b54d185aff151a4873a66e88fcc8
CarlosDevSys <cdealmeidavieira@gmail.com>
2024-05-06 11:26:04 -0300

    Realizada codigicação do projeto conforme descrição do desafio Dio Controle de fluxo

--- DesafioControleFluxo/src/com/carlosdevsys/controleFluxo/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/com/carlosdevsys/controleFluxo/ParametrosInvalidosException.java
@@ -1,5 +1,11 @@
 package com.carlosdevsys.controleFluxo;
 
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}      
 
 }
